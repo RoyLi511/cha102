5a35ea97cd828d060d4fc8dee262e87b61cbba2c
Roy <rrr111999o5@gmail.com>
2023-07-28 14:50:27 +0800

    Project initialize

---
+++ src/main/java/cha102/TestController.java
@@ -0,0 +1,27 @@
+package cha102;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		var xxx = req.getParameter("nickname");
+//		System.out.println(xxx);
+		req.setAttribute("nickname", xxx);
+		req.getRequestDispatcher("/index.jsp").forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
